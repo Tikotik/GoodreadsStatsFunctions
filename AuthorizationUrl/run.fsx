bdbfcf84731498cd8d66d6fa9bc8ccc667224aa6
mveith <mveith@hotmail.cz>
2017-01-14 20:01:49 +0100

    AuthorizationUrl - parametrized client side url

--- AuthorizationUrl/run.fsx
+++ AuthorizationUrl/run.fsx
@@ -9,13 +9,17 @@ open GoodreadsStats.Model
 open Newtonsoft.Json
 open System.Configuration
 
-let clientSideUrl = "http://localhost:1234" 
-
 let Run(req: HttpRequestMessage, log: TraceWriter) =
-    let clientKey = ConfigurationManager.AppSettings.["clientKey"]
-    let clientSecret = ConfigurationManager.AppSettings.["clientSecret"]
-
     async {
+        let clientKey = ConfigurationManager.AppSettings.["clientKey"]
+        let clientSecret = ConfigurationManager.AppSettings.["clientSecret"]
+
+        let queryValue key = 
+            let pair = req.GetQueryNameValuePairs() |> Seq.find (fun q -> q.Key = key)
+            pair.Value
+        
+        let clientSideUrl = queryValue "clientSideUrl"
+
         let (authorizationUrl, token, tokenSecret) = getAuthorizationData clientKey clientSecret clientSideUrl
         let result = JsonConvert.SerializeObject { Token = token; TokenSecret = tokenSecret; Url = authorizationUrl}
         log.Info(authorizationUrl)
