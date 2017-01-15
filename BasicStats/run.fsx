9ebe23a771368e3a845f580f5ca5c3b05f608041
mveith <mveith@hotmail.cz>
2017-01-15 20:11:18 +0100

    GoodreadsApi upgrade - removed FSharp.Data dependency

--- BasicStats/run.fsx
+++ BasicStats/run.fsx
@@ -10,25 +10,26 @@ open GoodreadsStats.Model
 open Newtonsoft.Json
 open System.Configuration
 open BasicStatsCalculator
+open Model
 
 let reviews accessData = 
     let user = getUser accessData
     getAllReviews accessData user.Id "read" "date_read"
 
-let createBook (r : Reviews.Review) = 
-    { ReadAt = parseOptionDate r.ReadAt
-      StartedAt = parseOptionDate r.StartedAt
+let createBook (r : Review) = 
+    let author = r.Book.Authors |> Seq.head
+    { ReadAt = r.ReadAt
+      StartedAt = r.StartedAt
       NumPages = r.Book.NumPages
       Book = 
           { Title = r.Book.Title
-            Author = r.Book.Author.Name } }
+            Author = author.Name } }
 
 let Run(req: HttpRequestMessage, log: TraceWriter) =
     let clientKey = ConfigurationManager.AppSettings.["clientKey"]
     let clientSecret = ConfigurationManager.AppSettings.["clientSecret"]
 
     async {
-
         try 
             let queryValue key = 
                 let pair = req.GetQueryNameValuePairs() |> Seq.find (fun q -> q.Key = key)
