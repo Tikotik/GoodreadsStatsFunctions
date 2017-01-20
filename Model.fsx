33186ee40823a88d9606695533194ce365f01c21
mveith <mveith@hotmail.cz>
2017-01-21 00:19:09 +0100

    BasicStats function removed

--- Model.fsx
+++ Model.fsx
@@ -2,23 +2,6 @@
 
 open System
 
-type Book = 
-    { Title : string
-      Author : string }
-
-type BookData = 
-    { Book : Book
-      PagesCount : int
-      DaysCount : int }
-
-type BasicStats = 
-    { BooksCount : int
-      PagesCount : int
-      SlowestBook : BookData
-      FastestBook : BookData
-      AverageSpeed : double
-      AveragePagesCount : double }
-      
 type AuthorizationUserData = 
     { Token : string
       TokenSecret : string
