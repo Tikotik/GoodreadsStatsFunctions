f32e1df92f925e2786ff6591c8417124b1bee72c
mveith <mveith@hotmail.cz>
2017-02-11 20:29:00 +0100

    Languages map in text file - azure functions cannot process big seq of tuples?!

--- Configuration.fsx
+++ Configuration.fsx
@@ -4,3 +4,4 @@ open System.Configuration
 
 let clientKey = ConfigurationManager.AppSettings.["clientKey"]
 let clientSecret = ConfigurationManager.AppSettings.["clientSecret"]
+let languagesFile = """D:\home\site\wwwroot\Languages.txt"""
